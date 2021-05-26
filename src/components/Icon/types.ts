649ce51b93cb86a663d973b07eacaaac09638bb3
Bamidele Isaac <bamideleisaac11@gmail.com>
2021-05-26 12:51:57 +0100

    fixed error

--- src/components/Icon/types.ts
+++
@@ -1,6 +0,0 @@
-export interface IProps {
-    icon: React.FC<React.SVGProps<SVGSVGElement>>
-    color?: string
-    size?: number;
-    text?:string
-  }
---
+++ src/pages/Auth/Login/style.ts
@@ -0,0 +1,12 @@
+
+import {styled} from "globalStyles";
+
+export const Login = styled.div`
+    justify-content:center;
+    margin:auto;
+    width:400px;
+`
+export const Form = styled.form`
+    display:block;
+    padding:0 20px;
+`
